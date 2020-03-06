04721e75cf549a05f44d8bf719d9e3490bf2d098
robertbemmann <bemmrob@gmail.com>
2020-03-06 22:40:37 +0100

    add code for consumer.py and line.py

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -29,7 +29,7 @@ class Turnstile(Producer):
         # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
         # replicas
         #
-        topic_name = "com.udacity.cta.turnstiles" # TODO: Come up with a better topic name
+        topic_name = "com.udacity.cta.turnstiles.v1" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Turnstile.key_schema,
